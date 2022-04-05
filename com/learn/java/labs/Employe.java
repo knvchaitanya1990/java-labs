a43112ef52894705cfd58fa208be09cfa50eaa51
knvchaitanya1990 <chaitanya.kattunga@gmail.com>
2022-04-05 18:48:06 +0530

    added getters & setter with proper exception handling
    added getters & setter with proper exception handling

---
+++ com/learn/java/labs/Employe.java
@@ -0,0 +1,72 @@
+package com.learn.java.labs;
+import java.util.Scanner;
+class Employee {
+    private String name;
+    private int age;
+    private int salary;
+    public Employee(String name, int age, int salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(" ", 123, 0);
+        try{
+            System.out.println("You entered Employee Details (name,age,salary)");
+            // Using Scanner for Getting Input from User
+            Scanner in = new Scanner(System.in);
+            String name = in.nextLine();
+            if(name.isEmpty())
+                System.out.println("Name can't be blank");
+            else
+                emp1.setName(name);
+            int age = in.nextInt();
+            if(!(age>0) && !(age < 120))
+                System.out.println("Age Should be between 0 & 120");
+            else
+                emp1.setAge(age);
+            int sal = in.nextInt();
+            if(!(sal>0))
+                System.out.println("Salary must be more than Zero");
+            else
+                emp1.setSalary(sal);
+            System.out.println(emp1.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
--- com/learn/java/labs/MatheMatics.java
+++ com/learn/java/labs/MatheMatics.java
@@ -2,10 +2,10 @@ package com.learn.java.labs;
 
 public class MatheMatics {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
 
         System.out.println((23-3+(3*9)*9));
-        System.out.println((17-7)*6+2+56);
+        System.out.println((17-7)/0*6+2+56);
         System.out.println(10+8*90-9-4);
     }
 }
